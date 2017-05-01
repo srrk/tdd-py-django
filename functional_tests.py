3ea306a1d9db44f7fd6fed4895a2f5c6c25247a9
SR Rajesh Kumar <rajeshux@gmail.com>
2017-05-01 14:21:59 +0530

    Handles one item in to-do

--- functional_tests.py
+++ functional_tests.py
@@ -46,7 +46,22 @@ class NewVisitorTest(unittest.TestCase):
                 "New to-do item did not appear in table"
         )
 
-        # There is ...
+        # There is still a text box inviting her to add another item.
+        # She enters 'Use peacock feathers to make a fly' (Bobby is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+                '2: Use peacock feathers to make a fly',
+                [row.text for row in rows]
+        )
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
